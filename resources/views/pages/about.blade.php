1a2726c0920a4c80c9a5cd2d60d1194be4c2063b
kjp44 <Sabbath51>
2018-10-23 19:43:06 -0400

    Laravel setup

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+   </br>
+   </br>
+   </br>
+   <h1>Welcome to my about page!</h1>
+@stop
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+@section('content')
+   </br>
+   </br>
+   </br>
+   <h1>Welcome to my home page!</h1>
+@stop
